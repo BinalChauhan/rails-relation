64dfc1a4dbd2345f8e3ce74d58903191f1bea5ec
Binal Chauhan <binal.essence@gmail.com>
2022-09-30 12:01:58 +0530

    Implement stripe using checkout and services.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,16 +11,6 @@ class OrdersController < ApplicationController
         @order = Order.new
     end
 
-    def create
-        @order = Order.create!(order_pera)
-        p @order
-        if @order.save
-            redirect_to products_path
-        else
-            render :new, status: :unprocessable_entity
-        end
-    end
-
     private
     def order_pera
         params.require(:order).permit(:address, :phone_no, :product_id, :account_id)
@@ -29,7 +19,7 @@ class OrdersController < ApplicationController
     def find_product
         @product = Product.find_by(id: params[:product_id])
     end
-
+    
     def find_account
         @account = Account.find_by(id: params[:account_id])
     end
---
+++ db/migrate/20220928051409_removeidfrom_account.rb
@@ -0,0 +1,5 @@
+class RemoveidfromAccount < ActiveRecord::Migration[6.1]
+  def change
+      remove_column :accounts, :customer_id, :integer
+  end
+end
